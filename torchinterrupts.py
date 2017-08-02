7c76528ad5ade00332782d512a354aee1f5bd173
Andrew Silva <asilvaa9@gmail.com>
2017-08-02 11:12:28 -0400

    modularized and stopped training on test set -.-

--- torchinterrupts.py
+++ torchinterrupts.py
@@ -32,89 +32,90 @@ def non_shuffling_train_test_split(X, y, test_size=0.2):
 def save_checkpoint(state, filename='checkpoint_rel_small075.pth.tar'):
     torch.save(state, filename)
 
-
-annots_dir = '/home/asilva/Data/int_annotations/my_rel_data/'
-X = []
-Y = []
-bad_keys = ['bb_y', 'bb_x', 'gaze_timestamp', 'pos_frame', 'send_timestamp', 'bb_height', 'pos_timestamp',
-            'timestamp', 'bb_width', 'objects_timestamp', 'pos_z', 'pos_x', 'pos_y', 'name', 'pose_timestamp',
-            'objects']
-for filename in os.listdir(annots_dir):
-    if os.path.isdir(os.path.join(annots_dir, filename)):
-            continue
-    af = open(os.path.join(annots_dir, filename), 'rb')
-    annotation = pickle.load(af)
-    # For each person in the segmentation
-    for person in annotation:
-        sorted_ann = sorted(annotation[person], key = lambda x: x['timestamp'])
-
-        for piece in sorted_ann:
-            new_input_data = []
-            for key, value in piece.iteritems():
-                if key in bad_keys:
-                    continue
-                if value >= 1.7e300:
-                    piece[key] = -5
-            label = piece['value']
-            if label == 0:
-                # last_value = -1
+def import_rel_data():
+    annots_dir = '/home/asilva/Data/int_annotations/my_rel_data/'
+    X = []
+    Y = []
+    bad_keys = ['bb_y', 'bb_x', 'gaze_timestamp', 'pos_frame', 'send_timestamp', 'bb_height', 'pos_timestamp',
+                'timestamp', 'bb_width', 'objects_timestamp', 'pos_z', 'pos_x', 'pos_y', 'name', 'pose_timestamp',
+                'objects']
+    for filename in os.listdir(annots_dir):
+        if os.path.isdir(os.path.join(annots_dir, filename)):
                 continue
-            new_input_data.append(piece['right_wrist_angle'])       # 0
-            new_input_data.append(piece['right_elbow_angle'])       # 1
-            new_input_data.append(piece['left_wrist_angle'])        # 2
-            new_input_data.append(piece['left_elbow_angle'])        # 3
-            # new_input_data.append(piece['shoulder_vec_x'])          # 4
-            # new_input_data.append(piece['shoulder_vec_y'])          # 5
-            new_input_data.append(piece['left_eye_angle'])          # 6
-            new_input_data.append(piece['right_eye_angle'])         # 7
-            # new_input_data.append(piece['eye_vec_x'])               # 8
-            # new_input_data.append(piece['eye_vec_y'])               # 9
-            new_input_data.append(piece['right_shoulder_angle'])    # 10
-            new_input_data.append(piece['left_shoulder_angle'])     # 11
-            new_input_data.append(piece['nose_vec_y'])              # 12
-            new_input_data.append(piece['nose_vec_x'])              # 13
-            # new_input_data.append(piece['left_arm_vec_x'])          # 14
-            # new_input_data.append(piece['left_arm_vec_y'])          # 15
-            # new_input_data.append(piece['right_arm_vec_x'])         # 16
-            # new_input_data.append(piece['right_arm_vec_y'])         # 17
-            new_input_data.append(piece['gaze'])                    # 18
-            new_input_data.append(0)                                # book 19
-            new_input_data.append(0)                                # bottle 20
-            new_input_data.append(0)                                # bowl 21
-            new_input_data.append(0)                                # cup 22
-            new_input_data.append(0)                                # laptop 23
-            new_input_data.append(0)                                # cell phone 24
-            new_input_data.append(0)                                # blocks 25
-            new_input_data.append(0)                                # tablet 26
-            new_input_data.append(0)                                # unknown 27
-            foi = 11 # first object index to make it easier to change stuff
-            for item in piece['objects']:
-                if item == 'book':
-                    new_input_data[foi] += 1
-                elif item == 'bottle':
-                    new_input_data[foi+1] += 1
-                elif item == 'bowl':
-                    new_input_data[foi+2] += 1
-                elif item == 'cup':
-                    new_input_data[foi+3] += 1
-                elif item == 'laptop':
-                    new_input_data[foi+4] += 1
-                elif item == 'cell phone':
-                    new_input_data[foi+5] += 1
-                elif item == 'blocks':
-                    new_input_data[foi+6] += 1
-                elif item == 'tablet':
-                    new_input_data[foi+7] += 1
+        af = open(os.path.join(annots_dir, filename), 'rb')
+        annotation = pickle.load(af)
+        # For each person in the segmentation
+        for person in annotation:
+            sorted_ann = sorted(annotation[person], key = lambda x: x['timestamp'])
+
+            for piece in sorted_ann:
+                new_input_data = []
+                for key, value in piece.iteritems():
+                    if key in bad_keys:
+                        continue
+                    if value >= 1.7e300:
+                        piece[key] = -5
+                label = piece['value']
+                if label == 0:
+                    # last_value = -1
+                    continue
+                new_input_data.append(piece['right_wrist_angle'])       # 0
+                new_input_data.append(piece['right_elbow_angle'])       # 1
+                new_input_data.append(piece['left_wrist_angle'])        # 2
+                new_input_data.append(piece['left_elbow_angle'])        # 3
+                # new_input_data.append(piece['shoulder_vec_x'])          # 4
+                # new_input_data.append(piece['shoulder_vec_y'])          # 5
+                new_input_data.append(piece['left_eye_angle'])          # 6
+                new_input_data.append(piece['right_eye_angle'])         # 7
+                # new_input_data.append(piece['eye_vec_x'])               # 8
+                # new_input_data.append(piece['eye_vec_y'])               # 9
+                new_input_data.append(piece['right_shoulder_angle'])    # 10
+                new_input_data.append(piece['left_shoulder_angle'])     # 11
+                new_input_data.append(piece['nose_vec_y'])              # 12
+                new_input_data.append(piece['nose_vec_x'])              # 13
+                # new_input_data.append(piece['left_arm_vec_x'])          # 14
+                # new_input_data.append(piece['left_arm_vec_y'])          # 15
+                # new_input_data.append(piece['right_arm_vec_x'])         # 16
+                # new_input_data.append(piece['right_arm_vec_y'])         # 17
+                new_input_data.append(piece['gaze'])                    # 18
+                new_input_data.append(0)                                # book 19
+                new_input_data.append(0)                                # bottle 20
+                new_input_data.append(0)                                # bowl 21
+                new_input_data.append(0)                                # cup 22
+                new_input_data.append(0)                                # laptop 23
+                new_input_data.append(0)                                # cell phone 24
+                new_input_data.append(0)                                # blocks 25
+                new_input_data.append(0)                                # tablet 26
+                new_input_data.append(0)                                # unknown 27
+                foi = 11 # first object index to make it easier to change stuff
+                for item in piece['objects']:
+                    if item == 'book':
+                        new_input_data[foi] += 1
+                    elif item == 'bottle':
+                        new_input_data[foi+1] += 1
+                    elif item == 'bowl':
+                        new_input_data[foi+2] += 1
+                    elif item == 'cup':
+                        new_input_data[foi+3] += 1
+                    elif item == 'laptop':
+                        new_input_data[foi+4] += 1
+                    elif item == 'cell phone':
+                        new_input_data[foi+5] += 1
+                    elif item == 'blocks':
+                        new_input_data[foi+6] += 1
+                    elif item == 'tablet':
+                        new_input_data[foi+7] += 1
+                    else:
+                        new_input_data[foi+8] += 1
+                # if sum(new_input_data) < 0:
+                #     continue
+                if label <= 2:
+                    label = 0
                 else:
-                    new_input_data[foi+8] += 1
-            # if sum(new_input_data) < 0:
-            #     continue
-            if label <= 2:
-                label = 0
-            else:
-                label = 1
-            X.append(new_input_data)
-            Y.append(label)
+                    label = 1
+                X.append(new_input_data)
+                Y.append(label)
+    return X, Y
 
 
 # annots_dir = '/Users/andrewsilva/my_annots_trimmed'
@@ -225,15 +226,15 @@ class Net(nn.Module):
 # loss_fn = torch.nn.CrossEntropyLoss(size_average=False)
 
 
-def train(epoch):
+def train(model, train_loader, loss_fn, optimizer, epoch):
     model.train()
     avg_loss = 0
     count = 0
     for iteration, batch in enumerate(train_loader, 1):
-    	if use_gpu:
-        	data, target = Variable(batch[0].cuda()), Variable(batch[1].cuda())
+        if use_gpu:
+            data, target = Variable(batch[0].cuda()), Variable(batch[1].cuda())
         else:
-        	data, target = Variable(batch[0]), Variable(batch[1])
+            data, target = Variable(batch[0]), Variable(batch[1])
 
         # output = model(data)
         loss = loss_fn(model(data), target)
@@ -247,7 +248,7 @@ def train(epoch):
     return avg_loss/count
 
 
-def test(epoch):
+def test(model, test_loader, loss_fn, optimizer, epoch):
     model.eval()
     test_loss = 0
     correct = 0
@@ -256,16 +257,16 @@ def test(epoch):
     false_zero = 0
     false_one = 0
     for iteration, batch in enumerate(test_loader, 1):
-    	if use_gpu:
-        	data, target = Variable(batch[0].cuda()), Variable(batch[1].cuda())
+        if use_gpu:
+            data, target = Variable(batch[0].cuda()), Variable(batch[1].cuda())
         else:
-        	data, target = Variable(batch[0]), Variable(batch[1])
+            data, target = Variable(batch[0]), Variable(batch[1])
         output = model(data)
         loss = loss_fn(output, target)
         test_loss += loss.data[0]
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        # optimizer.zero_grad()
+        # loss.backward()
+        # optimizer.step()
         for i in range(int(output.size()[0])):
             pred = output.data.max(1)[1][i]
             correct += pred.eq(target.data[i]).cpu().sum()
@@ -298,61 +299,74 @@ def test(epoch):
 #     real_x_train.append(X_train[index])
 # for index in range(len(X_test)):
 #     real_x_test.append(X_test[index])
-kf = StratifiedKFold(n_splits=5, shuffle=False)
 
-fold_number = 1
-for train_idx, test_idx in kf.split(X, Y):
-    real_x_train = []
-    real_x_test = []
-    y_train = []
-    y_test = []
-    if use_gpu:
-        model = Net().cuda()
-    else:
-        model = Net()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    if use_gpu:
-        loss_fn = torch.nn.NLLLoss(weight=torch.cuda.FloatTensor([1, 5]), size_average=False)
-    else:
-        loss_fn = torch.nn.NLLLoss(weight=torch.FloatTensor([1, 5]), size_average=False)
+def run_cross_val(X, Y, filename='checkpoint_rel_small075.pth.tar'):
+    kf = StratifiedKFold(n_splits=5, shuffle=False)
 
-    for idx in train_idx:
-        real_x_train.append(X[idx])
-        y_train.append(Y[idx])
-    for idx in test_idx:
-        real_x_test.append(X[idx])
-        y_test.append(Y[idx])
-    if use_gpu:
-    	real_x_train = torch.cuda.FloatTensor(real_x_train)
-    	y_train = torch.cuda.LongTensor(y_train)
-    	real_x_test = torch.cuda.FloatTensor(real_x_test)
-    	y_test = torch.cuda.LongTensor(y_test)
-    else:
-    	real_x_train = torch.Tensor(real_x_train)
-    	y_train = torch.LongTensor(y_train)
-    	real_x_test = torch.FloatTensor(real_x_test)
-    	y_test = torch.LongTensor(y_test)
+    fold_number = 1
+    for train_idx, test_idx in kf.split(X, Y):
+        real_x_train = []
+        real_x_test = []
+        y_train = []
+        y_test = []
+        if use_gpu:
+            model = Net().cuda()
+        else:
+            model = Net()
 
-    train_dat = data_utils.TensorDataset(real_x_train, y_train)
-    train_loader = data_utils.DataLoader(train_dat, batch_size=2, shuffle=False)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        if use_gpu:
+            loss_fn = torch.nn.NLLLoss(weight=torch.cuda.FloatTensor([1, 5]), size_average=False)
+        else:
+            loss_fn = torch.nn.NLLLoss(weight=torch.FloatTensor([1, 5]), size_average=False)
 
-    
-    test_dat = data_utils.TensorDataset(real_x_test, y_test)
-    test_loader = data_utils.DataLoader(test_dat, batch_size=2, shuffle=False)
+        for idx in train_idx:
+            real_x_train.append(X[idx])
+            y_train.append(Y[idx])
+        for idx in test_idx:
+            real_x_test.append(X[idx])
+            y_test.append(Y[idx])
+        if use_gpu:
+            real_x_train = torch.cuda.FloatTensor(real_x_train)
+            y_train = torch.cuda.LongTensor(y_train)
+            real_x_test = torch.cuda.FloatTensor(real_x_test)
+            y_test = torch.cuda.LongTensor(y_test)
+        else:
+            real_x_train = torch.Tensor(real_x_train)
+            y_train = torch.LongTensor(y_train)
+            real_x_test = torch.FloatTensor(real_x_test)
+            y_test = torch.LongTensor(y_test)
 
-    train_loss = []
-    test_loss = []
-    for epoch in range(epochs):
-    	if epoch % 50 == 0:
-        	print 'Fold#: ', fold_number
-        train_loss.append(train(epoch))
-        test_loss.append(test(epoch))
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict(),
-        })
-    fold_number += 1
-    # plt.plot(train_loss, 'r', test_loss, 'b')
-    # plt.show()
+        train_dat = data_utils.TensorDataset(real_x_train, y_train)
+        train_loader = data_utils.DataLoader(train_dat, batch_size=2, shuffle=False)
+
+
+        test_dat = data_utils.TensorDataset(real_x_test, y_test)
+        test_loader = data_utils.DataLoader(test_dat, batch_size=2, shuffle=False)
+
+        train_loss = []
+        test_loss = []
+        for epoch in range(epochs):
+            if epoch % 50 == 0:
+                print 'Fold#: ', fold_number
+            train_loss.append(train(model=model, train_loader=train_loader, loss_fn=loss_fn, optimizer=optimizer, epoch=epoch))
+            test_loss.append(test(model=model, test_loader=test_loader, loss_fn=loss_fn, optimizer=optimizer, epoch=epoch))
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, filename=filename)
+        fold_number += 1
+        # plt.plot(train_loss, 'r', test_loss, 'b')
+        # plt.show()
+
+
+def load_and_test(filename='checkpoint_rel_small075.pth.tar'):
+    pass
+
+
+if __name__ == '__main__':
+    model_name = 'checkpoint_rel_small075.pth.tar'
+    X, Y = import_rel_data()
+    run_cross_val(X, Y)
